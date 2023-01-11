63681401c4e9d5d229ba051fa2b550558e6fc0a2
Agustina <81659547+Bras1994@users.noreply.github.com>
2023-01-11 17:02:08 -0300

    Add files via upload

---
+++ .github/workflows/info.js
@@ -0,0 +1,137 @@
+var info = [ 
+    {
+        nombre:'I. El Mago',
+        img:'images/card1.jpg',
+        descripcion:'El mago mira fuera de la carta hacia nuestros sueños.El número Uno en el sentido pitagórico representa la primera fuerza primordial, la existencia primigenia creadora.',
+        match: 1,
+    },
+    {
+        nombre:'II.La Sacerdotisa',
+        img:'images/card2.jpg',
+        descripcion:'Las dos columnas del porche representan los dos principios, activo y pasivo que conforman la Sabiduría.El dos representa los opuestos complementarios necesarios para el orden cósmico.',
+        match: 2,
+
+    },
+    {
+        nombre:'III. La Emperatriz',
+        img:'images/card3.jpg',
+        descripcion:'Simboliza la vida familiar, la familia, la madre, la fertilidad.Representante de la fortaleza femenina, sexualmente atractiva al tiempo que poderosa. El tres representa así un número sagrado en casi todas las religiones del mundo',
+        match: 3,
+    },
+    {
+        nombre:'IV.El Emperador',
+        img:'images/card4.jpg',
+        descripcion:'Representa la solidez.El número del Emperador es el Cuatro, que según el tarot, representa la solidez; cuatro puntos cardinales, cuatro estaciones del año, cuatro paredes del Templo, etc., el cuatro es generalmente asociado con un ciclo entero y completo ',
+        match: 4,
+    },
+    {
+        nombre:'V. El Sumo Sacerdote ',
+        img:'images/card5.jpg',
+        descripcion:'Representa todas las estructuras que defienden sistemas de creencias.Simboliza la humanidad.El numero 5 se asocia con los planetas, con el proceso de interacción y con el magnetismo',
+        match: 5,
+    },
+    {
+        nombre:'VI. Los enamorados',
+        img:'images/card6.jpg',
+        descripcion:'Esto suele asociarse con la disyuntiva. Invertido suele asociarse con una mala decisión o con la indecisión destructiva. El número 6 está generalmente asociado con la necesidad de elección, de los impulsos sexuales y románticos.',
+        match: 6,
+    },
+    {
+        nombre:'VII. El Carro',
+        img:'images/card7.jpg',
+        descripcion:'El Carro representa el control de la mente humana sobre las pasiones animales y el instinto, o más esotéricamente, el Yo Superior.Representa la guerraanto contra fuerza superiores como contra los defectos internos del individuo.',
+        match: 7,
+    },
+    {
+        nombre:'VIII La Justicia ',
+        img:'images/card8.jpg',
+        descripcion:'Su significado difiere dependiendo de su posición: de forma normal, significa el sostén de la fuerza moral e integridad. De forma inversa significa el perjuicio, intolerancia, falsos testimonios, abuso, fanatismo, severidad en el juicio, injusticia',
+        match: 8,
+    },
+    {
+        nombre:'IX. El Ermitaño',
+        img:'images/card9.jpg',
+        descripcion:'Representa la introspección, la meditación en solitario, la necesidad de autoconocimiento, de retiro del mundo para entender mejor lo aprendido y asimilarlo.',
+        match: 9,
+    },
+    {
+        nombre:'X. La Rueda ',
+        img:'images/card10.jpg',
+        descripcion:'Soy una descripción',
+        match: 10,
+    },
+    {
+        nombre:'XI. La Fuerza ',
+        img:'images/card11.jpg',
+        descripcion:'imboliza que la voluntad que se antepone a cualquier problema, para encauzarlo y solucionarlo.',
+        match: 11,
+    },
+    {
+        nombre:'XII. El Colgado ',
+        img:'images/card12.jpg',
+        descripcion:'Asociado con el autosacrificio y la paciencia ante las adversidades. ',
+        match: 12,
+    },
+    {
+        nombre:'XIII. La Muerte ',
+        img:'images/card13.jpg',
+        descripcion:'no representa literalmente la muerte física y no necesariamente es un vaticinio negativo. La Muerte presenta el cambio (de acuerdo con A.E. Waite y Eden Gray), el fin de un ciclo y el resurgimiento de otro.',
+        match: 13,
+    },
+    {
+        nombre:'XIV. La Templanza',
+        img:'images/card14.jpg',
+        descripcion:'Representa al ser humano que está atado a los deseos materiales, los vicios y la materia. Suele representar el materialismo, la lujuria, la degradación y los excesos.',
+        match: 14,
+    },
+    {
+        nombre:'XV. El Diablo',
+        img:'images/card15.jpg',
+        descripcion:'Suele representar el materialismo, la lujuria, la degradación y los excesos.',
+        match: 15,
+    },
+    {
+        nombre:'XVI. La Torre',
+        img:'images/card16.jpg',
+        descripcion:'Se asocia la Torre con el caos, la catástrofe y la ruina.Sin embargo, su alegorismo más positivo es que la Torre representa las fuerzas divinas impulsando a la persona, sacándola de su comodidad.',
+        match: 16,
+    },
+    {
+        nombre:'XVII. La Estrella',
+        img:'images/card17.jpg',
+        descripcion:'Evoca a la paz interior y exterior, la espiritualidad, y armonía.En los momentos de oscuridad muestra que existe un camino para salir de eso.',
+        match: 17,
+    },
+    {
+        nombre:'XVIII. La Luna',
+        img:'images/card18.jpg',
+        descripcion:'Está asociada con la entidad atómica desconocida, la seguridad en la oscuridad y con el asiento del alma y con la eternidad.',
+        match: 18,
+    },
+    {
+        nombre:'XIX. El Sol',
+        img:'images/card19.jpg',
+        descripcion:'Refleja felicidad y satisfacción, vitalidad, autoconfianza y éxito.Representa cosas buenas y resultados positivos para las luchas actuales.',
+        match: 19,
+    },
+    {
+        nombre:'XX. El Juicio',
+        img:'images/card20.jpg',
+        descripcion:'Revela una transformación, un cambio que implica una decisión definitiva sobre el pasado en vista a una nueva planificación del futuro.',
+        match: 20,
+    },
+    {
+        nombre:'XXI. El Mundo',
+        img:'images/card21.jpg',
+        descripcion:'Representativo del mundo natural o el reino de las bestias.Los cinco elementos.',
+        match: 21,
+    },
+    {
+        nombre:'0.El Loco',
+        img:'images/card0.jpg',
+        descripcion:'Está asociado con el cuánto y con la dualidad tiempo/espacio.Es, en definitiva, una figura que empuja hacia la vida de modo espontáneo, saltándose las protecciones conscientes y conservadoras.',
+        match: 0,
+    }
+    
+
+]
